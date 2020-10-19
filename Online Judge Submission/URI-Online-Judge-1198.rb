87df93de4a3b68848249b5ab65f90ea0d7e9110a
sayanta28 <sayanta@baiust.edu.bd>
2020-10-19 12:04:33 +0600

    Update Commit

--- Online Judge Submission/URI-Online-Judge-1198.rb
+++ Online Judge Submission/URI-Online-Judge-1198.rb
@@ -1,15 +1,14 @@
-loop do
+
     hashmatSolder = gets.to_i #take input as integer
     enemySolder = gets.to_i #take input as integer
     
 
     if(hashmatSolder <= enemySolder) 
         res = enemySolder - hashmatSolder
-        puts "#{res}"
+        puts "#{res}\n"
     
     else 
         res = hashmatSolder-enemySolder
-        puts "#{res}"
+        puts "#{res}\n"
     
     end
-end
