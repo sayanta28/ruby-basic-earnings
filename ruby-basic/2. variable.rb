f41e0def1a1b8c9a6734f430c48bc2eca0e97ee9
sayanta28 <sayanta@baiust.edu.bd>
2020-11-11 21:50:54 +0600

    Update

--- ruby-basic/2. variable.rb
+++ ruby-basic/2. variable.rb
@@ -1,1 +1,20 @@
 #Variable is a container for hold the values.
+
+character_name = "Sayanta"
+character_age = "25"
+#puts "There once was a man named " + character_name
+#puts ("He was " + character_age + " years old")
+character_name = "Anik"
+#puts ("He really liked the name " + character_name)
+#puts("but didn't like being " + character_age ) 
+
+#String
+name = "Ashok"
+age = 75 #Integer
+gpa = 3.56 #Float
+isMale = true
+isTall = false
+flaws = nil
+#+ age + " " +gpa + " " + isMale + " " +isTall + " " + flaws)
+
+puts name + " "+ {age}
---
+++ ruby-basic/3. String.rb
@@ -0,0 +1,18 @@
+name = " Sayan Chy "
+puts name.class
+
+puts name.upcase()
+puts name.downcase()
+puts name.strip.downcase() #Cut space
+puts name.length()
+puts name.include? " Ch" #Cheack substr and return true / false
+puts name[2] #Access by index
+puts name[2,5]
+puts name.index('ya')
+puts "all".upcase()
+puts name.size == 10 #check length
+puts name.empty?
+age = 10
+puts "Hi #{name} #{age}" #It is called string interpolation
+
+
